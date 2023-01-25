b86033f6deb32aae22d63d9334c3078ec8cbe9e5
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-25 02:13:07 +0200

    Update Section.jsx

--- src/components/Section/Section.jsx
+++ src/components/Section/Section.jsx
@@ -1,36 +1,15 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { motion } from 'framer-motion';
-import styles from './Section.module.css';
 
-const variants = {
-  hidden: {
-    opacity: 0,
-    scale: 0.9,
-  },
-  visible: {
-    opacity: 1,
-    scale: 1,
-  },
+export const Section = ({ title, children }) => {
+  return (
+    <section>
+        {title && <h2>{title}</h2>}
+        {children}
+    </section>
+  );
 };
-
-const Section = ({ title, children }) => (
-  <section className={styles.container}>
-    <motion.h2
-      initial="hidden"
-      animate="visible"
-      variants={variants}
-      className={styles.container__title}
-    >
-      {title}
-    </motion.h2>
-    {children}
-  </section>
-);
 
 Section.propTypes = {
   title: PropTypes.string,
-  children: PropTypes.node,
-};
-
-export default Section;
+  children: PropTypes.node.isRequired
+}
