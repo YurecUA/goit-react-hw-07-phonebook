19b9fe8c6c8f4c89a2e3d1398d1c9ca66fd5b335
YurecUA <81627173+YurecUA@users.noreply.github.com>
2023-01-25 02:13:33 +0200

    Update store.js

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,49 +1,10 @@
-import {
-  persistStore,
-  persistReducer,
-  FLUSH,
-  REHYDRATE,
-  PAUSE,
-  PERSIST,
-  PURGE,
-  REGISTER,
-} from 'redux-persist';
-import {
-  configureStore,
-  getDefaultMiddleware,
-  combineReducers,
-} from '@reduxjs/toolkit';
-import storage from 'redux-persist/lib/storage';
+import { configureStore } from '@reduxjs/toolkit'
+import { filterReducer } from './filterSlice';
+import { contactsReducer } from './contactsSlice'
 
-import contactsReducer from './contacts/contacts-reducer';
-
-const contactsPersistConfig = {
-  key: 'contacts',
-  storage,
-  // исключаю ненужные свойства из сохранения локалсторейджа
-  blacklist: ['filter'],
-};
-
-const middleware = [
-  // лайфхак для удаления ошибки персиста
-  ...getDefaultMiddleware({
-    serializableCheck: {
-      ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-    },
-  }),
-];
-
-const rootReducer = combineReducers({
-  contacts: persistReducer(contactsPersistConfig, contactsReducer),
+export const store = configureStore({
+  reducer: {
+    contacts: contactsReducer,
+    filter: filterReducer
+  }
 });
-
-const store = configureStore({
-  reducer: rootReducer,
-  devTools: process.env.NODE_ENV === 'development',
-  middleware,
-});
-
-const persistor = persistStore(store);
-
-// eslint-disable-next-line import/no-anonymous-default-export
-export default { store, persistor };
